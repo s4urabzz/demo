31640210ae62f4e3bbb1de8201e17fa3170de952
Saurabh Nehul <55999382+saurabhnehul@users.noreply.github.com>
2022-01-30 22:27:39 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#add implementation
+def add(x,y):
+    pass
+ 
+#subtract implementation
+def subtract(x,y):
+    pass
+
+#multiply implementation
+def multiply(x,y):
+    pass    
+
+#divide implementation
+def divide(x,y):
+    pass   
+    
+def square(x) :
+    pass
